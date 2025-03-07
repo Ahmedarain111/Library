ab0b2dc143425ea4ec2f8f1351391598e41a808b
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-03-07 19:19:59 +0500

    refactor code into classes

--- script.js
+++ script.js
@@ -1,97 +1,88 @@
-const myLibrary = [];
+class Book {
+    constructor(name, author, pages, read = false) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
+
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    addBook(name, author, pages, read) {
+        const newBook = new Book(name, author, pages, read);
+        this.books.push(newBook);
+        this.displayBooks();
+    }
+
+    removeBook(index) {
+        this.books.splice(index, 1);
+        this.displayBooks();
+    }
+
+    toggleRead(index) {
+        this.books[index].read = !this.books[index].read;
+        this.displayBooks();
+    }
+
+    displayBooks() {
+        bookContainer.innerHTML = '';
+        this.books.forEach((book, index) => {
+            bookContainer.appendChild(this.createBookNode(book, index));
+        });
+    }
+
+    createBookNode(book, index) {
+        const bookElement = document.createElement('div');
+        bookElement.classList = "card";
+
+        bookElement.innerHTML = `
+            <p><b>${book.name}</b></p>
+            <p>by <b>${book.author}</b></p>
+            <p>${book.pages} pages</p>
+        `;
+
+        const readBtn = document.createElement('button');
+        readBtn.classList = 'submit';
+        readBtn.style.backgroundColor = book.read ? "green" : "red";
+        readBtn.innerText = book.read ? "Read" : "Not Read";
+
+        readBtn.addEventListener('click', () => this.toggleRead(index));
+
+        const removeBtn = document.createElement('button');
+        removeBtn.innerHTML = "Remove";
+        removeBtn.classList = 'submit';
+        removeBtn.addEventListener('click', () => this.removeBook(index));
+
+        bookElement.appendChild(readBtn);
+        bookElement.appendChild(removeBtn);
+
+        return bookElement;
+    }
+}
+
+
 const bookContainer = document.querySelector('.books-container');
 const dialog = document.querySelector('dialog');
 const addButton = document.querySelector('button');
 const form = document.querySelector('form');
+const library = new Library();
 
 addButton.addEventListener('click', () => dialog.showModal());
+
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    addBookToLibrary();
-})
 
+    const name = document.querySelector('#bookName').value;
+    const author = document.querySelector('#bookAuthor').value;
+    const pages = document.querySelector('#bookPages').value;
+    const read = document.querySelector('#readCheck').checked;
 
-function Book(name, author, pages, read = false) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-
-}
-
-function addBookToLibrary() {
-    const name = document.querySelector('#bookName');
-    const author = document.querySelector('#bookAuthor');
-    const pages = document.querySelector('#bookPages');
-    const read = document.querySelector('#readCheck');
-    let r = false;
-
-    if (read.value == 'on') r = true;
-    const book = new Book(name.value, author.value, pages.value, r)
-    myLibrary.push(book);
-
+    library.addBook(name, author, pages, read);
+    
+    form.reset();
     dialog.close();
-    displayBooks();
-
-    name.value = ''
-    author.value = ''
-    pages.value = ''
-    read.value = ''
-}
-
-function createBookNode(name, author, pages, read) {
-    const b = document.createElement('div');
-    const removeBtn = document.createElement('button');
-    const readBtn = document.createElement('button');
-
-    removeBtn.innerHTML = "Remove";
-
-    b.innerHTML =
-        `<p><b>${name}</b></p>
-    <p>by <b>${author}</b></p>
-    <p>${pages} pages</p>`;
-
-    b.classList = "card";
-    removeBtn.classList = 'submit';
-    readBtn.classList = 'submit'
-    readBtn.style.backgroundColor = read ? "green" : "red";
-    readBtn.innerText = read ? "Read" : "Not Read";
-
-
-    readBtn.addEventListener('click', () => {
-        if (readBtn.style.backgroundColor === "green") {
-            readBtn.innerText = "Not read";
-            readBtn.style.backgroundColor = 'red';
-        }
-        else {
-            readBtn.innerText = "Read";
-            readBtn.style.backgroundColor = 'green';
-        }
-    });
-
-    removeBtn.addEventListener('click', function () {
-        const index = myLibrary.findIndex(book =>
-            book.name === name && book.author === author && book.pages == pages && book.read === read
-        );
-
-        if (index !== -1) {
-            myLibrary.splice(index, 1);
-        }
-
-        b.remove();
-    });
-
-    b.appendChild(readBtn);
-    b.appendChild(removeBtn);
-
-    return b;
-}
-
-function displayBooks() {
-    bookContainer.innerHTML = '';
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        bookContainer.appendChild(createBookNode(myLibrary[i].name, myLibrary[i].author,
-            myLibrary[i].pages, myLibrary[i].read));
-    }
-}
+});
